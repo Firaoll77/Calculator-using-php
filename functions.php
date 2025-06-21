906f6f41bb3e6aa099539b6fb122677274b80907
Firasync <161107983+Firaoll77@users.noreply.github.com>
2025-06-21 15:03:12 +0300

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+
+    
+    function myCalculator($num01, $operator, $num02){
+        $sum;
+        switch ($operator) {
+            case "add":
+                $sum = $num01 + $num02;
+                break;
+            case "sub": 
+                $sum = $num01 - $num02;
+                break;
+             case "mult":
+                $sum = $num01 * $num02;
+                break;
+            case "div":
+                $sum = $num01/$num02;
+                break;
+            default:
+                $sum = "There was an error !!";
+                break;
+        }
+        return $sum;
+    }
+    $num01 = $_GET["num01"];
+    $operator = $_GET["operator"];
+    $num02 = $_GET["num02"];
+
+    echo "Value: " .myCalculator($num01,$operator,$num02);
+?>
